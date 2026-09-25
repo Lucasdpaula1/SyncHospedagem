894d5674313e3e530974cd0af22c7b086e574215
unknown <lucassilvadpaula@gmail.com>
2026-09-25 18:26:58 -0300

    fix: the inver

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,24 +1,2 @@
 
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'staysync_user') THEN
-      CREATE ROLE staysync_user WITH LOGIN PASSWORD 'staysync_pass123';
-   END IF;
-END
-$$;
-
-
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'staysync_db') THEN
-      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE staysync_db');
-   END IF;
-END
-$$;
-
-
-ALTER DATABASE staysync_db OWNER TO staysync_user;
 GRANT ALL PRIVILEGES ON DATABASE staysync_db TO staysync_user;
-
-
-\c staysync_db
